40db37f4537816d23604533cbcffc6e9a737ced9
M.NIKO NUR CAHYONO <mnikoncy@gmail.com>
2026-05-26 12:06:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,16 @@ include 'config.php';
 try {
  $pdo = new PDO($dsn, $user, $pass);
  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- echo "Koneksi berhasil";
+ echo "Koneksi berhasil". "<br>";
+
+ $query = "SELECT * FROM products";
+ $stmt = $pdo->query($query);
+ $products = $stmt->fetchAll();
+ 
+ foreach ($products as $product) {
+    echo $product['names'] . "<br>";
+ } 
+ 
 } catch (PDOException $e) {
     echo "Koneksi gagal: " . $e->getMessage();
 }
